11da78bb84daa3d73e45c76b27d828db51e1fb36
iftakhar <iftakharalamshuvo11@gmail.com>
2024-04-13 10:45:24 +0600

    user profile added

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Helmet } from "react-helmet-async";
+import { AuthContext } from "../components/authProvider/AuthProvider";
+import { Link } from "react-router-dom";
 
 const UserProfile = () => {
+  const { user } = useContext(AuthContext);
+  console.log(user);
   return (
-    <div>
+    <div className="min-h-screen flex flex-col justify-center items-center">
       <Helmet>
         <title>Little Hotelier | User Profile</title>
       </Helmet>
-      This is user profile
+      <p className="md:text-3xl text-xl font-black:">User Profile</p>
+      <h1 className="mb-10 md:text-3xl text-xl font-black"> Name: {user?.displayName || "user"}</h1>
+      <div className="card w-96 glass ">
+        <figure>
+          <img
+          className="w-full"
+            src={user?.photoURL || <p>user</p>}
+            alt="car!"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">User Details</h2>
+          <div>
+            <p>User Name:Name: {user?.displayName || "user"}</p>
+          <p>User Email:{user?.email || "dummy@dummy.com"}</p>
+          <p>User Phone Number : {user?.phoneNumber||"01*********"}</p>
+          </div>
+         
+          
+          <div className="card-actions ">
+           <Link to={"/update"}> <button className="btn bg-violet-400 hover:bg-blue-300 hover:text-white">Update Profile</button></Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
